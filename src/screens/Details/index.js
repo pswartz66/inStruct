53158c703f26f4941012a86c4f01ec97a6fc61aa
Phil Swartz <philswartz13@gmail.com>
2021-05-08 12:25:00 -0400

    built out details screen

--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import styles from './styles';
+import { Entypo } from '@expo/vector-icons';
+
 
 const DetailsScreen = (props) => {
 
@@ -11,14 +13,91 @@ const DetailsScreen = (props) => {
   // fetch all instructors from database skilled in "data" 
   // & approximately close to the person location
   // and list vertically using a flat list
-
+  const tempData = [
+    {
+      id: 1,
+      name: 'Joe',
+      skill: 'basketball'
+    },
+    {
+      id: 2,
+      name: 'Jake',
+      skill: 'finance'
+    },
+    {
+      id: 3,
+      name: 'Shawn',
+      skill: 'microsoft excel'
+    },
+    {
+      id: 4,
+      name: 'Nicki',
+      skill: 'painting'
+    },
+    {
+      id: 5,
+      name: 'Alisha',
+      skill: 'drawing'
+    },
+    {
+      id: 5,
+      name: 'Alisha',
+      skill: 'drawing'
+    },
+    {
+      id: 5,
+      name: 'Alisha',
+      skill: 'drawing'
+    },
+    {
+      id: 5,
+      name: 'Alisha',
+      skill: 'drawing'
+    },
+    {
+      id: 5,
+      name: 'Alisha',
+      skill: 'drawing'
+    },
+  ]
 
   return (
-    <View style={styles.DetailsContainer}>
-      <View style={styles.DetailHeaderRow}>
-        <Text style={styles.DetailsHeaderLeft}>Skilled in</Text>
-        <Text style={styles.DetailsHeaderRight}> {data}.</Text>
-      </View>
+    <View style={styles.detailsContainer}>
+      <ScrollView>
+        <View style={styles.detailHeaderRow}>
+          <Text style={styles.detailsHeaderLeft}>Skilled in</Text>
+          <Text style={styles.detailsHeaderRight}> {data}.</Text>
+        </View>
+
+        <View style={styles.detailDataList}>
+          {tempData.map((item) => (
+
+            <View style={styles.detailCards}>
+
+              <View style={styles.detailsCardInner}>
+
+                <View style={styles.detailsProfImg}></View>
+
+                <View style={styles.detailsBottomCard}>
+
+
+                <Text style={{ fontSize: 16, fontWeight: '500' }}>
+                  {item.name}
+                </Text>
+
+
+                  <Entypo name="plus" size={24} color="#2679ff" />
+                </View>
+
+
+
+              </View>
+
+            </View>
+
+          ))}
+        </View>
+      </ScrollView>
     </View>
   )
 }
--- src/screens/Details/styles.js
+++ src/screens/Details/styles.js
@@ -1,25 +1,68 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  DetailsContainer: {
+  detailsContainer: {
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: 'white',
   },
-  DetailHeaderRow: {
+  detailHeaderRow: {
     flexDirection: 'row',
-    marginTop: 20,
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 10,
   },
-  DetailsHeaderLeft: {
+  detailsHeaderLeft: {
     color: 'black',
     fontFamily: 'HelveticaNeue-Medium',
     fontSize: 22,
   },
-  DetailsHeaderRight: {
+  detailsHeaderRight: {
     color: '#2679ff',
     fontFamily: 'HelveticaNeue-Medium',
     fontSize: 22,
+  },
+  detailDataList: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: 'white',
+    width: Dimensions.get('window').width,
+    padding: 5,
+  },
+  detailCards: {
+    height: 200,
+    width: '50%',
+    margin: 0,
+    shadowColor: 'black',
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: .2
+  },
+  detailsCardInner: {
+    height: 180,
+    width: '90%',
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    margin: 10,
+  },
+  detailsProfImg: {
+    height: 100,
+    width: '100%',
+    borderRadius: 6,
+    backgroundColor: 'gray',
+    marginBottom: 6,
+  },
+  detailsBottomCard: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignSelf: 'flex-end',
+    alignItems: 'center',
   }
 });
 
